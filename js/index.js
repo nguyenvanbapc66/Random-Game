8e2f8c9f5862d6f029e10cdcaf8983c2f0854c60
vanba278 <nguyenvanbapc66@gmail.com>
2020-04-17 16:35:05 +0700

    finish the project

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            numberOfGuessing: 0
+        }
+    }
+
+    updateNumberOfGuessing = (newValue) => {
+        this.setState({
+            numberOfGuessing: newValue
+        })
+    }
+
     render() {
         return (
             <div>
-                <Header/>
-                <Body/>
+                <Header numberOfGuessing={this.state.numberOfGuessing} />
+                <Body updateNumberOfGuessing={this.updateNumberOfGuessing} numberOfGuessing={this.state.numberOfGuessing} />
             </div>
-        )
+        );
     }
 }
 
@@ -15,84 +28,93 @@ class Header extends React.Component {
             <div className="header jumbotron text-center">
                 <h2>Guessing random number</h2>
                 <p>
-                    Tôi đã chọn một số random trong khoảng
-                    1 đến 100, bạn có thể đoán được?
-                </p>    
+                Tôi đã chọn một số random trong khoảng 1 đến 100, bạn có thể đoán
+                được?
+                </p>
+                {
+                    this.props.numberOfGuessing >= 7 && (
+                        <p className="text-danger">
+                            Số lần bạn đoán khá cao: {this.props.numberOfGuessing}. 
+                            Hãy cẩn thận không là thua!
+                        </p>
+                    )
+                }
             </div>
-        )
+        );
     }
 }
 
 class Body extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            numberOfGuessing: 0,
             inputValue: 0,
+            maximumNumberGuessing: 10,
             randomNumber: this.randomNumber(),
-            message: ''
+            message: "",
         }
     }
 
     // New game
     newGame = (e) => {
         this.setState({
-            numberOfGuessing: 0,
             inputValue: 0,
             maximumNumberGuessing: 10,
             randomNumber: this.randomNumber(),
-            message: ''
-        })
-    }
+            message: "",
+        });
+    };
 
     // Get random number between 1 and 100
     randomNumber = () => {
-        return Math.floor(Math.random()*100 + 1)
-    }
+        return Math.floor(Math.random() * 100 + 1);
+    };
 
     // Event of input value change
     onValueChane = (e) => {
-        // console.log(e.target.value)
         this.setState({
-            ...this.state, inputValue: parseInt(e.target.value)
-        })
-    }
-
-    enter = (e) => {
-        console.log(e.target)
-        this.guessing
-    }
+            ...this.state,
+            inputValue: parseInt(e.target.value),
+        });
+    };
 
     guessing = () => {
-        const {inputValue, randomNumber, numberOfGuessing} = this.state
-        const newNumberOfGuessing = numberOfGuessing + 1
-        if(inputValue < randomNumber) {
-            this.setState({...this.state, numberOfGuessing: newNumberOfGuessing, message: "Your number is too small"})
-        } else if(inputValue > randomNumber){
-            this.setState({...this.state,numberOfGuessing: newNumberOfGuessing, message: "Your number is too big"})
-        } else{
-            alert("You win with score: " + newNumberOfGuessing)
-            this.newGame()
+        const {inputValue, randomNumber} = this.state;
+        const newNumberOfGuessing = this.props.numberOfGuessing + 1;
+        this.props.updateNumberOfGuessing(newNumberOfGuessing)
+        if (inputValue < randomNumber) {
+            this.setState({
+                ...this.state, message: "Your number is too small",
+            });
+        } else if (inputValue > randomNumber) {
+            this.setState({
+                ...this.state, message: "Your number is too big",
+            });
+        } else {
+            alert("You win with score: " + newNumberOfGuessing);
+            this.props.updateNumberOfGuessing('0')
+            this.newGame();
         }
-        if(newNumberOfGuessing >= this.state.maximunNumberGuessing) {
-            alert("You lose!")
-            this.newGame()
+        if (newNumberOfGuessing >= this.state.maximumNumberGuessing) {
+            alert("You lose!");
+            this.props.updateNumberOfGuessing('0')
+            this.newGame();
         }
-    }
+    };
 
     render() {
         return (
             <div className="body">
                 <button className="btn btn-primary" onClick={this.newGame}>New Game</button>
-                <hr/>
-                <p className="text-bold">Số lần bạn đã đoán là: {this.state.numberOfGuessing}</p>
+                <hr />
+                <p className="text-bold">Số lần bạn đã đoán là: {this.props.numberOfGuessing}</p>
                 <p className="text-bold">Số bạn đoán là: </p>
-                <input type="number" max="100" value={this.state.inputValue} onChange={this.onValueChane}/>
+                <input type="number" max="100" value={this.state.inputValue} onChange={this.onValueChane} />
                 <button className="btn bg-success text-white" onClick={this.guessing}>Đoán</button>
                 <p>{this.state.message}</p>
             </div>
-        )
+        );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
